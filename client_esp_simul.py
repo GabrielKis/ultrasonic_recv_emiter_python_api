f0e940a8a5ce623080e3b8bbb622f26837b2b4e2
gkis <gabrielkis65@gmail.com>
2021-05-22 20:10:22 -0300

    clients basic working

--- client_esp_simul.py
+++ client_esp_simul.py
@@ -5,17 +5,37 @@ import time
 mqttBroker ="mqtt.eclipseprojects.io" 
 
 client_name = 'Client_ESP_Simul'
+topics_list = [('QTD', 2), ('Waveform', 2)]
 
 def on_message(client, userdata, message):
-    print("received message: " ,str(message.payload.decode("utf-8")))
+    print("{} RECEIVED {} from topic {}".format(client_name, str(message.topic), \
+                                            str(message.payload.decode("utf-8"))))
 
-client = mqtt.Client("Temperature_Inside_Getter", transport="websockets")
-client.connect(mqttBroker, port=80)
+def send_data(client_obj):
+    #client_pc = mqtt.Client(client_name, transport="tcp")
 
-client.loop_start()
+    #randNumber = uniform(20.0, 21.0)
+    client_obj.publish("Ultrasound_send", 'PAYLOAD1')
+    print("{} PUBLISH {} to Ultrasound_send".format(str(client_name), 'PAYLOAD1'))
+    client_obj.publish("Ultrasound_recv", 'PAYLOAD2')
+    print("{} PUBLISH {} to Ultrasound_recv".format(str(client_name), 'PAYLOAD2'))
 
-client.subscribe("CMD")
-client.on_message=on_message
+def receive_data(client_obj):
+    client_obj.loop_start()
 
-time.sleep(30)
-client.loop_stop()
+    client_obj.subscribe(topics_list)
+    client_obj.on_message=on_message
+
+    time.sleep(30)
+    client_obj.loop_stop()
+
+if __name__ == "__main__":
+    client_esp = mqtt.Client("Temperature_Inside_Getter", transport="websockets")
+    client_esp.connect(mqttBroker, port=80)
+    #parse input
+    #receive-waveforms
+    receive_data(client_esp)
+    #send cmd-waveform
+    send_data(client_esp)
+    client_esp.disconnect()
+    #show_data
--- client_pc_mqtt.py
+++ client_pc_mqtt.py
@@ -3,17 +3,40 @@ from random import randrange, uniform
 import time
 
 mqttBroker ="mqtt.eclipseprojects.io" 
-
 client_name = "Client_PC"
 
-#client_pc = mqtt.Client(client_name, transport="tcp")
-client_pc = mqtt.Client("Client_PC", transport="websockets")
-client_pc.connect(mqttBroker, port=80)
+topics_pub_list = [('QTD', 2), ('Waveform', 2)]
+topics_sub_list = [('Ultrasound_send', 2), ('Ultrasound_recv', 2)]
 
-for i in range(0,10):
+def on_message(client, userdata, message):
+    print("{} RECEIVED {} from topic {}".format(client_name, str(message.topic), \
+                                            str(message.payload.decode("utf-8"))))
+
+def send_data(client_obj):
+    #client_pc = mqtt.Client(client_name, transport="tcp")
+
     #randNumber = uniform(20.0, 21.0)
-    client_pc.publish("CMD", i)
-    print("{} PUBLISH {} to CMD".format(str(client_name), str(i)))
-    time.sleep(0.5)
+    client_obj.publish("QTD", 5)
+    print("{} PUBLISH {} to QTD".format(str(client_name), 5))
+    client_obj.publish("Waveform", 'SINE')
+    print("{} PUBLISH {} to Waveform".format(str(client_name), 'Sine'))
 
-client_pc.disconnect()
+def receive_data(client_obj):
+    client_obj.loop_start()
+
+    client_obj.subscribe(topics_sub_list)
+    client_obj.on_message=on_message
+
+    time.sleep(60)
+    client_obj.loop_stop()
+
+if __name__ == "__main__":
+    client_pc = mqtt.Client("Client_PC", transport="websockets")
+    client_pc.connect(mqttBroker, port=80)
+    #parse input
+    #send cmd-waveform
+    send_data(client_pc)
+    #receive-waveforms
+    receive_data(client_pc)
+    client_pc.disconnect()
+    #show_data
