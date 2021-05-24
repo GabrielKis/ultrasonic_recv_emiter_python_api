7f1328980f3e43d4156a6afd73c833c7587d0fd0
gkis <gabrielkis65@gmail.com>
2021-05-24 09:49:02 -0300

    Apos receber topicos requisitados, programa termina

--- client_esp_simul.py
+++ client_esp_simul.py
@@ -1,3 +1,4 @@
+import queue
 import paho.mqtt.client as mqtt 
 from random import randrange, uniform
 import time
@@ -9,20 +10,10 @@ mqttBroker ="mqtt.eclipseprojects.io"
 client_name = 'Client_ESP_Simul'
 topics_list = [('QTD', 2), ('Waveform', 2)]
 
-cmd_payloads = []
-wf_format_payloads = []
-wf_send_payloads = []
-wf_recv_payloads = []
-
 def on_message(client, userdata, message):
     q.put(message)
     print("{} RECEIVED {} from topic {}".format(client_name, str(message.topic), \
                                             str(message.payload.decode("utf-8"))))
-    if message.topic == 'CMD':
-        cmd_payloads.append(message.payload.decode("utf-8"))
-    elif message.topic == 'Waveform':
-        wf_format_payloads.append(message.payload.decode("utf-8"))
-
 def send_data(client_obj):
     client_obj.publish("Ultrasound_send", 'PAYLOAD1')
     print("{} PUBLISH {} to Ultrasound_send".format(str(client_name), 'PAYLOAD1'))
@@ -30,16 +21,28 @@ def send_data(client_obj):
     print("{} PUBLISH {} to Ultrasound_recv".format(str(client_name), 'PAYLOAD2'))
 
 def receive_data(client_obj):
+    receive_data_ctrl = [False, False]
+
     client_obj.loop_start()
 
     client_obj.subscribe(topics_list)
     client_obj.on_message=on_message
 
-    time.sleep(30)
+    # espera ate que os topicos de retorno sejam recebidos
+    while not all(receive_data_ctrl):
+        if not q.empty():
+            msg = q.get()
+            if msg.topic == 'QTD':
+                # salvar mensagem em alguma variavel
+                receive_data_ctrl[0] = True
+            if msg.topic == 'Waveform':
+                receive_data_ctrl[1] = True
+
     client_obj.loop_stop()
 
 if __name__ == "__main__":
     q = Queue()
+    receive_data_ctrl_queue = Queue()
     client_esp = mqtt.Client("Temperature_Inside_Getter", transport="websockets")
     client_esp.connect(mqttBroker, port=80)
     #parse input
@@ -49,8 +52,3 @@ if __name__ == "__main__":
     send_data(client_esp)
     client_esp.disconnect()
     #show_data
-
-    print(cmd_payloads)
-    print(wf_format_payloads)
-
-    print(q)
--- client_pc_mqtt.py
+++ client_pc_mqtt.py
@@ -2,6 +2,8 @@ import paho.mqtt.client as mqtt
 from random import randrange, uniform
 import time
 
+from queue import Queue
+
 mqttBroker ="mqtt.eclipseprojects.io" 
 client_name = "Client_PC"
 
@@ -9,6 +11,7 @@ topics_pub_list = [('QTD', 2), ('Waveform', 2)]
 topics_sub_list = [('Ultrasound_send', 2), ('Ultrasound_recv', 2)]
 
 def on_message(client, userdata, message):
+    q.put(message)
     print("{} RECEIVED {} from topic {}".format(client_name, str(message.topic), \
                                             str(message.payload.decode("utf-8"))))
 
@@ -22,15 +25,26 @@ def send_data(client_obj):
     print("{} PUBLISH {} to Waveform".format(str(client_name), 'Sine'))
 
 def receive_data(client_obj):
+    receive_data_ctrl = [False, False]
+
     client_obj.loop_start()
 
     client_obj.subscribe(topics_sub_list)
     client_obj.on_message=on_message
 
-    time.sleep(60)
+    # espera ate que os topicos de retorno sejam recebidos
+    while not all(receive_data_ctrl):
+        if not q.empty():
+            msg = q.get()
+            if msg.topic == topics_sub_list[0][0]:
+                receive_data_ctrl[0] = True
+            if msg.topic == topics_sub_list[1][0]:
+                receive_data_ctrl[1] = True
+
     client_obj.loop_stop()
 
 if __name__ == "__main__":
+    q = Queue()
     client_pc = mqtt.Client("Client_PC", transport="websockets")
     client_pc.connect(mqttBroker, port=80)
     #parse input
