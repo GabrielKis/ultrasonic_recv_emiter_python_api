d6c5c30d2538eafd7cdc20c11fed3efd126afe7e
gkis <gabrielkis65@gmail.com>
2021-05-29 09:58:02 -0300

    Transformado client_pc em Classe

--- client_pc_mqtt.py
+++ client_pc_mqtt.py
@@ -4,54 +4,65 @@ import time
 
 from queue import Queue
 
-#mqttBroker ="mqtt.eclipseprojects.io" 
-mqttBroker ="localhost" 
+mqttBroker ="192.168.4.2" 
 client_name = "Client_PC"
 
-topics_pub_list = [('QTD', 2), ('Waveform', 2)]
-topics_sub_list = [('Ultrasound_send', 2), ('Ultrasound_recv', 2)]
+topics_pub_list = [('config_to_esp', 2)]
+topics_sub_list = [('ultrasound_send', 2), ('ultrasound_recv', 2)]
 
-def on_message(client, userdata, message):
-    q.put(message)
-    print("{} RECEIVED {} from topic {}".format(client_name, str(message.topic), \
-                                            str(message.payload.decode("utf-8"))))
+class EspMqttComm():
+    def __init__(self, pc_client_name):
+        # send and receive params to esp
+        self.q = Queue()
+        self.client_pc = mqtt.Client(pc_client_name, transport="websockets")
+        #client_pc = mqtt.Client("Client_PC")
 
-def send_data(client_obj):
-    #client_pc = mqtt.Client(client_name, transport="tcp")
+    def connect(self):
+        self.client_pc.connect(mqttBroker, port=5089)
 
-    #randNumber = uniform(20.0, 21.0)
-    client_obj.publish("QTD", 5)
-    print("{} PUBLISH {} to QTD".format(str(client_name), 5))
-    client_obj.publish("Waveform", 'SINE')
-    print("{} PUBLISH {} to Waveform".format(str(client_name), 'Sine'))
+    def disconnect(self):
+        self.client_pc.disconnect()
 
-def receive_data(client_obj):
-    receive_data_ctrl = [False, False]
+    def send_command_to_esp(self, qtd_periods, waveform_data):
+        #parse input
+        payload = bytes([int(qtd_periods)]) + bytes(waveform_data)
+        #send cmd-waveform
+        self._send_data(payload)
+        #receive-waveforms
+        self._receive_data()
+        #show_data
 
-    client_obj.loop_start()
+    def _on_message(self, client, userdata, message):
+        self.q.put(message)
+        print("{} RECEIVED {} from topic: {}".format(client_name, str(message.topic), \
+                                                message.payload))
+        print(len(message.payload))
 
-    client_obj.subscribe(topics_sub_list)
-    client_obj.on_message=on_message
+    def _send_data(self, payload):
+        self.client_pc.publish(topics_pub_list[0][0], payload)
+        print("{} PUBLISH {} to {}".format(str(client_name), payload, topics_pub_list[0][0]))
 
-    # espera ate que os topicos de retorno sejam recebidos
-    while not all(receive_data_ctrl):
-        if not q.empty():
-            msg = q.get()
-            if msg.topic == topics_sub_list[0][0]:
-                receive_data_ctrl[0] = True
-            if msg.topic == topics_sub_list[1][0]:
-                receive_data_ctrl[1] = True
+    def _receive_data(self):
+        receive_data_ctrl = [False, False]
 
-    client_obj.loop_stop()
+        self.client_pc.loop_start()
+
+        self.client_pc.subscribe(topics_sub_list)
+        self.client_pc.on_message=self._on_message
+
+        # espera ate que os topicos de retorno sejam recebidos
+        while not all(receive_data_ctrl):
+            if not self.q.empty():
+                msg = self.q.get()
+                if msg.topic == topics_sub_list[0][0]:
+                    receive_data_ctrl[0] = True
+                if msg.topic == topics_sub_list[1][0]:
+                    receive_data_ctrl[1] = True
+
+        self.client_pc.loop_stop()
 
 if __name__ == "__main__":
-    q = Queue()
-    client_pc = mqtt.Client("Client_PC", transport="websockets")
-    client_pc.connect(mqttBroker, port=9001)
-    #parse input
-    #send cmd-waveform
-    send_data(client_pc)
-    #receive-waveforms
-    receive_data(client_pc)
-    client_pc.disconnect()
-    #show_data
+    sonar_client_pc = EspMqttComm(client_name)
+    sonar_client_pc.connect()
+    sonar_client_pc.send_command_to_esp(1, [0xff,0xff,0xff])
+    sonar_client_pc.disconnect()
